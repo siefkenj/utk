a4ba99eef9820e37d39d6346075e581c3b0a84e1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-08-29 20:41:02 +0000

    Deploy siefkenj/utk to github.com/siefkenj/utk.git:gh-pages

---
+++ precache-manifest.525d8d9158bc4774570c4d6f8d9f0b53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6baf7be932fd69a57a07af72be61bf2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6590785980eed852a08b",
+    "url": "./static/css/2.0829903f.chunk.css"
+  },
+  {
+    "revision": "84bb6ff953c7044c1c04",
+    "url": "./static/css/main.5349e47d.chunk.css"
+  },
+  {
+    "revision": "6590785980eed852a08b",
+    "url": "./static/js/2.46ab06db.chunk.js"
+  },
+  {
+    "revision": "b7541124178a7019b1080ed4b4abbe5b",
+    "url": "./static/js/2.46ab06db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84bb6ff953c7044c1c04",
+    "url": "./static/js/main.bb457a2f.chunk.js"
+  },
+  {
+    "revision": "97aff048cb4e0fe75676",
+    "url": "./static/js/runtime-main.f986a51f.js"
+  }
+]);
